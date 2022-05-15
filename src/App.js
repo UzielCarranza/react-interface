5b1585583bdfb39122df7fe3673905f05768e6d8
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-15 13:52:24 -0400

    moved data Json to the public folder to simulate an outside API... used fetch to simulate the fetching processto get appointment data from an API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import {BiCalendar, BiTrash} from "react-icons/bi";
+import {BiCalendar} from "react-icons/bi";
 import Search from './components/Search';
 import AddAppointment from "./components/AddApointment";
 import AppointmentInfo from "./components/AppointmentInfo";
-import appointmentList from "./data.json";
-import {FaTrashAlt} from "react-icons/fa";
-
-
+import {useState, useCallback, useEffect} from "react";
 
 
 function App() {
+    let [appointmentList, setAppointmentList] = useState([]);
+
+    //retrieving data from outside of resources
+
+    const FetchData = useCallback(() => {
+        fetch('./data.json')
+            .then(res => res.json())
+            .then(data => {setAppointmentList(data)}
+            );
+    }, []);
+
+
+    // any changes that are done use effect will take care of it automatically
+    useEffect(() => {
+        FetchData();
+    }, [FetchData])
+
     return (
         <div className="App-container">
             <h1 className="app-h1 mb-3"><BiCalendar className="archive--icon"/>Your Appointments</h1>
