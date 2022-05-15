c168e4d8294dbd7c98ee3720a5326e3a1e6e11aa
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-15 10:38:38 -0400

    added add apoinment component to create and add apointments into the interface

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import {BiArchive} from "react-icons/bi";
+import { BiCalendar} from "react-icons/bi";
 import Search from './components/Search';
+import AddApointment from "./components/AddApointment";
 
 function App() {
     return (
         <div className="App-container">
-            <h1 className="app-h1"><BiArchive className="archive--icon"/>Your Appointments</h1>
+            <h1 className="app-h1 mb-3"><BiCalendar className="archive--icon"/>Your Appointments</h1>
+            <AddApointment/>
             <Search/>
         </div>
     );
