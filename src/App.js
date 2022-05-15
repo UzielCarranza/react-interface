66d10b80cb0e85bb5cff2d2096f36d416d9ee42e
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-15 16:18:02 -0400

    added functionality to dropdown menu

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
     let [appointmentList, setAppointmentList] = useState([]);
     let [query, setQuery] = useState("");
 
+    let [sortBy, setSortBy] = useState("petName")
+    let [orderBy, setOrderBy] = useState("asc")
+
     //retrieving data from outside of resources
 
     const filteredAppointments = appointmentList.filter(
@@ -16,20 +19,25 @@ function App() {
             return (
                 item.petName.toLowerCase().includes(query.toLowerCase()) ||
                 item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
-                item.aptNotes.toLowerCase().includes(query.toLowerCase())
-            )
+                item.appNotes.toLowerCase().includes(query.toLowerCase())
+            );
         }
-    )
+    ).sort((a, b) => {
+        let order = (orderBy === 'asc') ? 1 : -1
+        return (
+            a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+                ? -1 * order : 1 * order
+        )
+    })
 
     const FetchData = useCallback(() => {
         fetch('./data.json')
             .then(res => res.json())
             .then(data => {
-                setAppointmentList(data)
+                    setAppointmentList(data)
                 }
             );
     }, []);
-
 
 
     // any changes that are done use effect will take care of it automatically
@@ -42,8 +50,13 @@ function App() {
             <h1 className="app-h1 mb-3"><BiCalendar className="archive--icon"/>Your Appointments</h1>
             <AddAppointment/>
             <Search
-            query={query}
-            onQueryChange={myQuery => setQuery(myQuery)}/>
+                query={query}
+                onQueryChange={myQuery => setQuery(myQuery)}
+                orderBy={orderBy}
+                onOrderByChange={mySort => setOrderBy(mySort)}
+                sortBy={sortBy}
+                onSortByChange={sortBy => setSortBy(sortBy)}
+            />
 
             {/*//everytime you loop through a series of elements, you should use a key*/}
             <ul className="divide-y divide-gray-200">
